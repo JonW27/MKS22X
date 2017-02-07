b42c27057ce16d28df3fe7f6ba94f99690f9ee66
JonW27 <18wongj@gmail.com>
2017-02-06 22:07:28 -0500

    finished lab 1tested cases, should proceed as normal

---
+++ 01Recursion/Driver.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+import java.lang.Math;
+public class Driver{
+    public static void main(String[] args){
+        out.println(Recursion.name());
+        out.println(Recursion.sqrt(100));
+        out.println(Recursion.sqrt(37.5));
+        out.println(Recursion.sqrt(3.4));
+        out.println(Recursion.sqrt(0.5));
+        out.println(Recursion.sqrt(12.26));
+        out.println(Recursion.sqrt(Math.PI));
+        out.println(Recursion.sqrt(-5.4));
+    }
+}
---
+++ 01Recursion/Recursion.java
@@ -0,0 +1,20 @@
+public class Recursion{
+    public static String name(){
+        return "10.Wong.Jonathan";
+    }
+    public static double sqrt(double n){
+        if(n < 0){
+            throw new IllegalArgumentException();
+        }
+        return helper(n,1, -1);
+    }
+    private static double helper(double n, double guess, double former){
+        //System.out.println(guess);
+        return prox(n,guess) || guess == former ? guess : helper(n,((n/guess)+guess)/2, guess);
+    }
+    private static boolean prox(double n, double guess){
+        double threshold = 0.0000000000000000000000000000000000000000000000001;
+        double res = n / (guess * guess) - 1;
+        return res >= 0 ? res < threshold : (-1 * res) < threshold;
+    }
+}
