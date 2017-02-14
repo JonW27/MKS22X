0af2ac37ed903c9f01475bbaa264e477abe84e99
JonW27 <18wongj@gmail.com>
2017-02-13 23:08:29 -0500

    the first part is *almost* done, i promise (not)

--- 01Recursion/Driver.java
+++ 01Recursion/Driver.java
@@ -1,5 +1,6 @@
 import static java.lang.System.out;
 import java.lang.Math;
+import java.util.Random;
 public class Driver{
     public static void main(String[] args){
         out.println(Recursion.name());
@@ -11,5 +12,25 @@ public class Driver{
         out.println(Recursion.sqrt(Math.PI));
         //out.println(Recursion.sqrt(-5.4));
         out.println(Recursion.sqrt(1424546790));
+        out.println(Recursion.sqrt(144));
+        out.println(Recursion.sqrt(289));
+        out.println(Recursion.sqrt(341));
+        Random a = new Random();
+        int truths = 0;
+        int falses = 0;
+        int times = 50;
+        for (int i = 0; i<times; i++){
+            double b = Math.abs(a.nextInt(10000000));
+            double d = Math.round(Recursion.sqrt(b) * Recursion.sqrt(b));
+            boolean c = b == d;
+            out.println("Number: "+b+" Generated Result: "+Recursion.sqrt(b)+" Result: "+c);
+            if(c){
+                truths+=1;
+            }
+            else{
+                falses+=1;
+            }
+        }
+        System.out.println("Report:\nTrue "+truths+"\nFalse "+falses+"\nTotal "+times);
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MKS22X
 This is the repository for Stuyvesant's MKS22X class.
 All code in this repository is licensed under the [GPLv3](https://www.gnu.org/licenses/gpl-3.0.en.html) licence. 
+As deadlines approach, I realize the best approach to fixing problems may *just* be crowd-sourcing. As I have been allowed to place my code on Github, a code-sharing platform, and have open-sourced my work, I welcome pull requests to make my convoluted inefficient code better.
