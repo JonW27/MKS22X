044bb7ed733d702d70ce75c05dc3569eec60265b
JonW27 <18wongj@gmail.com>
2017-03-24 23:55:19 -0400

    working on dutch flag partition

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -1,7 +1,14 @@
 public class Driver{
     public static void main(String[] args){
-        int[] data = {8, 4, 3, 2, 0, 0, 1, 6, 6, 5, 5, 5, 5, 5};
-	System.out.println(Quick.partition(data, 0, data.length-1));
-        System.out.println(Quick.quickselect(data, 13));
+    //int[] data = {7, 32, 5, 5, 5, 4, 1, 2, 2, 2, 2, 6, 11, 11, 11, 11, 13, 8, -2, -9};
+	int[] data = {5, 1, 6, 12, 243, 3, 34, 3243, -21};
+    //int[] data= {2, 5, -1};
+    System.out.println(Quick.part(data, 0, data.length-1));
+    System.out.println("\n");
+    try{
+        System.out.println(Quick.quickselect(data, 0));
+    }catch(Throwable f){
+        System.out.println(f.toString());
+    }
     }
 }
--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Arrays;
 public class Quick{
     static Random rand = new Random();
 
@@ -8,29 +9,69 @@ public class Quick{
         data[second] = temp;
     }
 
-    public static int partition(int[] data, int start, int end){
-        int pivot = rand.nextInt(end-start) + start;
+    // modified to include dutch partitioning part
+    public static int part(int[] data, int start, int end){
+        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging
+        int v = data[pivot];
         int i = start;
-        swap(data, pivot, end);
-        while(start < end){
-            if(data[start] < data[end]){
-                swap(data, i, start);
+        int lt = start;
+        int gt = end;
+        swap(data, pivot, start);
+        while(i < gt){
+            if(data[i] == v){
                 i++;
             }
-            start++;
+            else if(data[i] < v){
+                swap(data, i, lt);
+                lt++;
+                i++;
+            }
+            else{
+                swap(data, i, gt);
+                gt--;
+            }
         }
-        swap(data, end, i);
-        return i;
+        return (lt + gt) / 2; // don't need modulo since java int division be like this
 
     }
+
+    // modified for quickselect
+    public static int[] qpart(int[] data, int start, int end){
+        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging
+        int v = data[pivot];
+        int i = start;
+        int lt = start;
+        int gt = end;
+        swap(data, pivot, start);
+        while(i < gt){
+            if(data[i] == v){
+                i++;
+            }
+            else if(data[i] < v){
+                swap(data, i, lt);
+                lt++;
+                i++;
+            }
+            else{
+                swap(data, i, gt);
+                gt--;
+            }
+        }
+        int[] jello = {i, lt, gt};
+        return jello; // don't need modulo since java int division be like this
+
+    }
+
+
     public static int quickselect(int[] data, int k){
 	return helper(data, k, 0, data.length-1);
    }
    private static int helper(int[] data, int k, int start, int end){
-	int partition = partition(data, start, end);
-	if(partition == k){
+	int[] partition = qpart(data, start, end);
+	if(k >= partition[1] && k <= partition[2]){
 		return data[k];
 	}
-	return (partition < k) ? helper(data, k, start, k) : helper(data, k, k, end);
+    //System.out.println(Arrays.toString(data));
+	return (partition[0] < k) ? helper(data, k, k, end) : helper(data, k, start, k);
   }
 }
