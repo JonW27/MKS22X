7b519aa4cf3fad3d1c4cf263d27b1cb6a118b2d7
JonW27 <18wongj@gmail.com>
2017-03-25 20:22:30 -0400

    partition seemingly works with everything but quicksort is prone to mess up at whim, but generate completely correct answers roughly 2/3s of the time. Me thinks this is a boundary error, an edge case, but if mr k is testing this on say ten test cases, then thats only a 66% on average, which is not good

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -2,8 +2,12 @@ public class Driver{
     public static void main(String[] args){
     //int[] data = {7, 32, 5, 5, 5, 4, 1, 2, 2, 2, 2, 6, 11, 11, 11, 11, 13, 8, -2, -9};
 	int[] data = {5, 1, 6, 12, 243, 3, 34, 3243, -21};
-    //int[] data= {2, 5, -1};
-    System.out.println(Quick.part(data, 0, data.length-1));
+    //int[] data = {-21, 1};
+    /*int[] samp = {3, 1, -21};*/
+    /*System.out.println(Quick.part(samp, 0, samp.length-1));*/
+    //int[] data= {1, 1, 1, 1, 1}; // not working for duplicates and negative numbers
+    //int[] data = {2, 8, 1};
+    System.out.println(Quick.part(data, 0, 0, data.length-1));
     System.out.println("\n");
     try{
         System.out.println(Quick.quickselect(data, 0));
--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -9,15 +9,18 @@ public class Quick{
         data[second] = temp;
     }
 
-    // modified to include dutch partitioning part
-    public static int part(int[] data, int start, int end){
+    // modified to include dutch partitioning part (as a result it includes int k)
+    public static int part(int[] data, int k, int start, int end){
         int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging
         int v = data[pivot];
         int i = start;
         int lt = start;
         int gt = end;
+        //System.out.println(Arrays.toString(data));
         swap(data, pivot, start);
-        while(i < gt){
+        while(i < gt+1){
+            //System.out.println(Arrays.toString(data));
+            //System.out.println("LOW BOUND: "+lt+"\n HIGH BOUND: "+gt);
             if(data[i] == v){
                 i++;
             }
@@ -29,36 +32,20 @@ public class Quick{
             else{
                 swap(data, i, gt);
                 gt--;
-            }
+            }   
         }
+        // stuff to prevent throwing of stackoverflow errors
+        if(k == 0 && lt == 1){
+            return (data[start+1] > data[start]) ? data[start] : data[start+1]; 
+        }
+        int[] part = new int[end-start+1];
+        System.arraycopy(data, 0, part, 0, end-start+1);
+        System.out.println("START: "+start+"\n END: "+end);
+        System.out.println("LOW BOUND: "+lt+"\n HIGH BOUND: "+gt);
+        System.out.println("PIVOT VAL: "+v+"\n");
+        System.out.println(Arrays.toString(part));
+        System.out.println((lt + gt) / 2);
         return (lt + gt) / 2; // don't need modulo since java int division be like this
-
-    }
-
-    // modified for quickselect
-    public static int[] qpart(int[] data, int start, int end){
-        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging
-        int v = data[pivot];
-        int i = start;
-        int lt = start;
-        int gt = end;
-        swap(data, pivot, start);
-        while(i < gt){
-            if(data[i] == v){
-                i++;
-            }
-            else if(data[i] < v){
-                swap(data, i, lt);
-                lt++;
-                i++;
-            }
-            else{
-                swap(data, i, gt);
-                gt--;
-            }
-        }
-        int[] jello = {i, lt, gt};
-        return jello; // don't need modulo since java int division be like this
 
     }
 
@@ -67,11 +54,10 @@ public class Quick{
 	return helper(data, k, 0, data.length-1);
    }
    private static int helper(int[] data, int k, int start, int end){
-	int[] partition = qpart(data, start, end);
-	if(k >= partition[1] && k <= partition[2]){
+	int partition = part(data, k, start, end);
+	if(partition == k){
 		return data[k];
 	}
-    //System.out.println(Arrays.toString(data));
-	return (partition[0] < k) ? helper(data, k, k, end) : helper(data, k, start, k);
+	return (partition < k) ? helper(data, k, partition+1, end) : helper(data, k, start, partition-1);
   }
 }
