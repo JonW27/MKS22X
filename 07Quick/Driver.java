0b134d527c2d38e93c4b1248290dd981b05756fb
JonW27 <18wongj@gmail.com>
2017-03-26 19:13:58 -0400

    quicksort works for everything

--- 07Quick/Driver.java
+++ 07Quick/Driver.java
@@ -1,18 +1,72 @@
+import java.util.Arrays;
+import java.lang.Integer;
+import java.util.Random;
 public class Driver{
     public static void main(String[] args){
     //int[] data = {7, 32, 5, 5, 5, 4, 1, 2, 2, 2, 2, 6, 11, 11, 11, 11, 13, 8, -2, -9};
-	int[] data = {5, 1, 6, 12, 243, 3, 34, 3243, -21};
+	int times = Integer.parseInt(args[0]);
+    int quicksort = Integer.parseInt(args[1]);
+    Random rand = new Random();
+    int pass = 0;
+    int fail = 0;
+    for(int i = 0; i < times; i++){
+        int length = rand.nextInt(1000000);
+        Random rand1 = new Random();
+        int[] data = new int[length];
+        for(int j = 0; j < length; j++){
+            if(rand1.nextInt(3) == 2){
+                data[j] = rand1.nextInt(1000) * -1;    
+            }
+            else{
+                data[j] = rand1.nextInt(1000);
+            }
+        }
+        int[] sorted = Arrays.copyOf(data, data.length);
+        Arrays.sort(sorted);
+        int xSmallestElement = rand.nextInt(length);
+        try{
+            long startTime = System.nanoTime();
+            if(sorted[xSmallestElement] == Quick.quickselect(data, xSmallestElement)){
+                //System.out.println("pass");
+                pass++;
+            }
+            else{
+                fail++;
+            }
+            long estimatedTime = (System.nanoTime() - startTime) / 1000000000;
+            System.out.println("QUICKSELECT time:"+ estimatedTime+ "s for length "+data.length);
+        }catch(Exception e){
+            System.out.println("an error was thrown for quickselect");
+            fail++;
+        }
+        try{
+            long startTime = System.nanoTime();
+            Quick.quicksort(data);
+            if(Arrays.equals(sorted, data)){
+                pass++;
+            }
+            long estimatedTime = (System.nanoTime() - startTime) / 1000000000;
+            System.out.println("QUICKSORT time: "+ estimatedTime+ "s for length "+data.length);
+        }catch(Exception e){
+            System.out.println("an error was thrown for quicksort");
+            fail++;
+        }
+    }
+    System.out.println("PASS: "+pass);
+    System.out.println("FAIL: "+fail);
+    //int[] data = {};
     //int[] data = {-21, 1};
     /*int[] samp = {3, 1, -21};*/
     /*System.out.println(Quick.part(samp, 0, samp.length-1));*/
     //int[] data= {1, 1, 1, 1, 1}; // not working for duplicates and negative numbers
     //int[] data = {2, 8, 1};
-    System.out.println(Quick.part(data, 0, 0, data.length-1));
-    System.out.println("\n");
-    try{
+    //System.out.println(Quick.part(data, 0, 0, data.length-1));
+    /*(try{
         System.out.println(Quick.quickselect(data, 0));
-    }catch(Throwable f){
-        System.out.println(f.toString());
-    }
+        Quick.quicksort(data);
+        System.out.println(Arrays.toString(data));
+    }catch(Exception f){
+        f.printStackTrace();
+    }*/
     }
 }
--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -8,17 +8,17 @@ public class Quick{
         data[first] = data[second];
         data[second] = temp;
     }
-
+    
     // modified to include dutch partitioning part (as a result it includes int k)
     public static int part(int[] data, int k, int start, int end){
-        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging
+        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging <- CHANGED
         int v = data[pivot];
         int i = start;
         int lt = start;
         int gt = end;
         //System.out.println(Arrays.toString(data));
         swap(data, pivot, start);
-        while(i < gt+1){
+        while(i < gt + 1){
             //System.out.println(Arrays.toString(data));
             //System.out.println("LOW BOUND: "+lt+"\n HIGH BOUND: "+gt);
             if(data[i] == v){
@@ -35,29 +35,73 @@ public class Quick{
             }   
         }
         // stuff to prevent throwing of stackoverflow errors
-        if(k == 0 && lt == 1){
+        if(k == 0 && lt == start+1){
             return (data[start+1] > data[start]) ? data[start] : data[start+1]; 
         }
-        int[] part = new int[end-start+1];
+        else if(k >= lt && k <= gt){ // this took the majority of the time, the && symbols
+            return data[k];
+        }
+        return (k > lt) ? part(data, k, gt, end) : part(data, k, start, lt-1);
+
+        /*int[] part = new int[end-start+1];
         System.arraycopy(data, 0, part, 0, end-start+1);
-        System.out.println("START: "+start+"\n END: "+end);
+        System.out.println("\nSTART: "+start+"\n END: "+end);
         System.out.println("LOW BOUND: "+lt+"\n HIGH BOUND: "+gt);
         System.out.println("PIVOT VAL: "+v+"\n");
         System.out.println(Arrays.toString(part));
         System.out.println((lt + gt) / 2);
-        return (lt + gt) / 2; // don't need modulo since java int division be like this
+        return (lt + gt) / 2;*/ // don't need modulo since java int division be like this
 
     }
 
 
-    public static int quickselect(int[] data, int k){
-	return helper(data, k, 0, data.length-1);
+    public static int quickselect(int[] ary, int k){
+        if(ary.length != 0){
+            return part(ary, k, 0, ary.length-1);
+        }
+        return 0; // can't select a value from a null set
    }
-   private static int helper(int[] data, int k, int start, int end){
-	int partition = part(data, k, start, end);
-	if(partition == k){
-		return data[k];
-	}
-	return (partition < k) ? helper(data, k, partition+1, end) : helper(data, k, start, partition-1);
-  }
+
+    public static void helper(int[] data, int start, int end){
+        if(start >= end){
+            return; // base case
+        }
+        //System.out.println("END: "+end+"START: "+start+"DIFF: "+(end-start));
+        int pivot = rand.nextInt(end-start) + start + 1; // this 1 literally took most of my time debugging <- CHANGED
+        int v = data[pivot];
+        int i = start;
+        int lt = start;
+        int gt = end;
+        //System.out.println(Arrays.toString(data));
+        swap(data, pivot, start);
+        while(i < gt + 1){
+            //System.out.println(Arrays.toString(data));
+            //System.out.println("LOW BOUND: "+lt+"\n HIGH BOUND: "+gt);
+            if(data[i] == v){
+                i++;
+            }
+            else if(data[i] < v){
+                swap(data, i, lt);
+                lt++;
+                i++;
+            }
+            else{
+                swap(data, i, gt);
+                gt--;
+            }   
+        }
+        if(lt > 0){
+            helper(data, start, lt - 1);
+        }
+        helper(data, gt, end);
+    }
+
+    public static void quicksort(int[] ary){
+        if(ary.length != 0){
+            helper(ary, 0, ary.length-1);
+        }
+        return; 
+    }
+
+
 }
