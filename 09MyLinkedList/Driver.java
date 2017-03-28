658f19ee5596d829907378758a38733701462e53
Jonw27 <18wongj@gmail.com>
2017-03-28 15:34:31 -0400

    push more scaffolding. im tired

---
+++ 09MyLinkedList/Driver.java
@@ -0,0 +1,6 @@
+public class Driver{
+	public static void main(String[] args){
+		MyLinkedList bob = new MyLinkedList();
+		bob.add(8);
+	}
+}
--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,15 +1,25 @@
+import java.lang.Integer;
 public class MyLinkedList{
     LNode start;
     int size;
-    public MyLinkedList{
-        public void add(){
-
-        }
-        public String toString(){
-            
-        }
+    public MyLinkedList(){
+	start = new LNode(null);
+	size = 0;
+    }
+    public boolean add(int value){
+	LNode current = start;
+	return true;
+    }
+    public String toString(){
+	return "for now...";
     }
     public class LNode{
-    
+	int value;
+	LNode next;
+	public LNode(Integer value){
+	    this.value = value;
+	    this.next = null;
+	}
+	    
     }
 }
