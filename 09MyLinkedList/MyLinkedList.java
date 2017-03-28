e96b1ed7ca6d2551f28542f60ac8d336f2724ceb
JonW27 <18wongj@gmail.com>
2017-03-27 22:25:47 -0400

    skeleton for 09. i will attempt to push more regularly instead of mass-pushing, or may automate it to do this automatically

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,15 @@
+public class MyLinkedList{
+    LNode start;
+    int size;
+    public MyLinkedList{
+        public void add(){
+
+        }
+        public String toString(){
+            
+        }
+    }
+    public class LNode{
+    
+    }
+}
