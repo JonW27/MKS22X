f196718b67aa7ce596467d0eafc57112b6b1059e
JonW27 <18wongj@gmail.com>
2017-04-02 23:47:01 -0400

    Looks likes its working. Will develop comprehensive test suite for further unit testing

--- 09MyLinkedList/Driver.java
+++ 09MyLinkedList/Driver.java
@@ -2,5 +2,22 @@ public class Driver{
 	public static void main(String[] args){
 		MyLinkedList bob = new MyLinkedList();
 		bob.add(8);
+		bob.add(7);
+		bob.add(-2);
+		bob.add(-11);
+		bob.add(0);
+		bob.add(-7);
+		bob.add(985);
+		bob.add(0);
+		System.out.println(bob.get(2));
+		System.out.println(bob);
+		bob.set(0, 5);
+		System.out.println(bob);
+		bob.set(3, -34);
+		bob.add(0,2); // index 0 gives problemos
+		System.out.println(bob);
+		bob.remove(0);
+		bob.remove(3);
+		System.out.println(bob);
 	}
 }
--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,25 +1,125 @@
-import java.lang.Integer;
 public class MyLinkedList{
-    LNode start;
-    int size;
-    public MyLinkedList(){
-	start = new LNode(null);
-	size = 0;
+  LNode start;
+  LNode end;
+  int size;
+
+  public MyLinkedList(){
+    size = 0;
+    start = new LNode();
+    end = start;
+  }
+  public boolean add(int value){
+    LNode temp = new LNode(value);
+    end.next = temp;
+    temp.prev = end;
+    end = temp;
+    size++;
+    return true;
+  }
+  public int size(){
+    return size;
+  }
+  public int get(int index){
+    if(index > size - 1 || index < 0){
+      throw new IndexOutOfBoundsException();
     }
-    public boolean add(int value){
-	LNode current = start;
-	return true;
+    LNode current = start;
+    for(int i = 0; i < index+1; i++){
+      current = current.next;
     }
-    public String toString(){
-	return "for now...";
+    return current.value;
+  }
+  public int set(int index, int newValue){
+    if(index > size -1 || index < 0){
+      throw new IndexOutOfBoundsException();
     }
-    public class LNode{
-	int value;
-	LNode next;
-	public LNode(Integer value){
-	    this.value = value;
-	    this.next = null;
-	}
-	    
+    LNode current = start;
+    for(int i = 0; i < index+1; i++){
+      current = current.next;
     }
+    int temp = current.value;
+    current.value = newValue;
+    return temp;
+  }
+  public int indexOf(int value){
+    if(size == 0){ // need to do this due to the creation of a LNode at the beginning for facilitated chaining
+      return -1;
+    }
+    LNode current = start;
+    current = current.next;
+    for(int i = 0; i < size - 1; i++){
+      if(current.value == value){
+        return i;
+      }
+      current = current.next;
+    }
+    return -1;
+  }
+  public void add(int index, int value){
+    if(index > size - 1 || index < 0){
+      throw new IndexOutOfBoundsException();
+    }
+    LNode current = start;
+    LNode temp = new LNode(value);
+    for(int i = 0; i < size; i++){
+      if(i == index){
+        temp.prev = current;
+      }
+      if(i == index + 1){
+        current.prev = temp;
+        temp.next = current;
+      }
+      current = current.next;
+    }
+    temp.prev.next = temp;
+    size++;
+  }
+  public int remove(int index){
+    if(index > size -1 || index < 0){
+      throw new IndexOutOfBoundsException();
+    }
+    LNode current = start;
+    int val = -1;
+    for(int i = 0; i < size; i++){
+      if(i == index + 1){
+        val = current.value;
+      }
+      if(i == index + 2){
+        current.prev = current.prev.prev;
+        current.prev.next = current;
+      }
+      current = current.next;
+    }
+    size--;
+    return val;
+  }
+  public String toString(){
+    if(size == 0){ // need to do this due to the creation of a LNode at the beginning for facilitated chaining
+      return "[]";
+    }
+    String result = "[";
+    LNode current = start;
+    current = current.next;
+    for(int i = 0; i < size - 1; i++){
+      result = result + current.value + ", ";
+      current = current.next;
+    }
+    result = result + current.value+"]";
+    return result;
+  }
+
+  private class LNode{
+    int value;
+    LNode next;
+    LNode prev;
+    public LNode(){
+
+    }
+    public LNode(int value){
+      this.value = value;
+      this.prev = null;
+      this.next = null;
+    }
+
+  }
 }
